74741a0c50e9649e5957804346fc1ea229e883d5
Wey <113145358+weyispalacamargo@users.noreply.github.com>
2023-03-01 23:44:11 +0000

    O mesmo texto só que com o format

--- nome_e_idade.py
+++ nome_e_idade.py
@@ -1,5 +1,7 @@
 nome = input("Digite o seu nome: ")
-print("Olá " + nome)
+#print("Olá " + nome)
+print(f"Olá {nome}")
 
 idade = input("Digite a sua idade: ")
-print("O(a) " + nome + " possui " + idade + " anos.")
+#print("O(a) " + nome + " possui " + idade + " anos.")
+print(f"O(a) {nome} possui {idade} anos.")
